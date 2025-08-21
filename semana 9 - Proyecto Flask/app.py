9531d5f463c86b64d7011c96701177e69df43b3a
KarenAg2000 <kj.aguindagr@uea.edu.ec>
2025-08-21 13:34:11 -0500

    semana10

--- semana 9 - Proyecto Flask/app.py
+++ semana 9 - Proyecto Flask/app.py
@@ -1,18 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '¡Hola! Bienvenido a mi aplicación Flask.'
+    return render_template('index.html')
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
-    return f'Bienvenido, {nombre}!'
+    return render_template('usuario.html', nombre=nombre)
 
 @app.route('/usuario')
 def usuario_fijo():
-    return 'Bienvenido, Karen Aguinda!'
+    return render_template('usuario.html', nombre="Karen Aguinda")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
